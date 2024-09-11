57aac631761a0800c85bd8f32dba9da65088b2a0
AdamAgerling <alltidvakenhehe@gmail.com>
2024-09-11 10:36:12 +0200

    Add project files.

---
+++ Labb01/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+// String to test: 29535123p48723487597645723645
+
+Console.WriteLine();
+Console.WriteLine(" ------------------------------------------------------------------------------------------------------------");
+Console.WriteLine(" |          The kind of string sequence the program is looking for: 29535123p48723487597645723645           |");
+Console.WriteLine(" |             Copy the one above and try it, or make your own sequence and try it yourself! :)             |");
+Console.WriteLine(" |                  The result of the sequence above should yield the sum: 5836428677242                    |");
+Console.WriteLine(" ------------------------------------------------------------------------------------------------------------");
+Console.WriteLine();
+
+
+static void NumberFinderInAString()
+    {
+    Console.Write("Input your string: ");
+
+    var userInput = Console.ReadLine();
+    Console.WriteLine();
+    long totalSum = 0;
+
+    for (int i = 0; i < userInput?.Length; i++)
+        {
+        for (int j = i + 1; j < userInput?.Length; j++)
+            {
+            string numberHighlighter = userInput.Substring(i, j - i + 1);
+            if (IsANumber(numberHighlighter) && numberHighlighter[0] == numberHighlighter[numberHighlighter.Length - 1])
+                {
+                Console.Write(userInput.Substring(0, i));
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(numberHighlighter);
+
+                Console.ResetColor();
+                Console.WriteLine(userInput.Substring(j + 1));
+
+                totalSum = totalSum + long.Parse(numberHighlighter);
+                }
+            }
+        }
+    Console.WriteLine();
+    Console.Write($"The total sum of all substrings added together is: ");
+
+    if (totalSum > 0)
+        {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write(totalSum);
+
+        }
+    else
+        {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(totalSum);
+        }
+    Console.ResetColor();
+    Console.WriteLine();
+    }
+
+static bool IsANumber(string userInput)
+    {
+    foreach (char letter in userInput)
+        {
+        if (!char.IsDigit(letter))
+            {
+            return false;
+            }
+        }
+    return true;
+    }
+
+NumberFinderInAString();
